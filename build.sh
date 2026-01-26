f375911423d010ac611d7f5d0fa2931771de8ab1
Henrik Forstén <henrik.forsten@gmail.com>
2026-01-26 20:09:08 +0200

    Rename files to be less generic

--- build.sh
+++ build.sh
@@ -4,26 +4,24 @@ set -e
 
 rm -rf dist
 mkdir dist
-mkdir dist/wasm_solver
 
 #make -C src/wasm_solver/
 
 # JS
-npx esbuild src/app.js \
+npx esbuild src/app_solver.js \
   --bundle \
   --minify \
   --format=esm \
   --sourcemap \
   --platform=node \
-  --outfile=dist/app.js
+  --outfile=dist/app_solver.js
 
 # CSS
-npx postcss src/style.css \
+npx postcss src/solver-style.css \
   --use cssnano \
-  -o dist/style.css
+  -o dist/solver-style.css
 
 # HTML
 cp src/field_solver.html dist/
 cp src/plotly-3.3.0.min.js dist/
-cp src/wasm_solver/solver.js dist/wasm_solver/solver.js
 cp src/wasm_solver/solver.wasm dist/solver.wasm
